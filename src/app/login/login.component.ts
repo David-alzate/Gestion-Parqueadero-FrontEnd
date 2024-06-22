ff5551a3015084447fe8246e8b0d4e3feb807dbe
David-alzate <alzatedavid0126@gmail.com>
2024-06-21 22:04:21 -0500

    Se Actualizo el formulario de inicio de sesion para que el usuario ingrese segun su tipo de empleado y numero de identificacion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TipoEmpleadosService } from '../services/tipoEmpleados/tipo-empleados.service';
 import { LoginService } from '../services/login/login.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
@@ -9,38 +10,50 @@ import { AuthService } from '../services/auth/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  tipoEmpleado: any;
 
   constructor(
     public fb: FormBuilder,
+    public tipoEmpleadoService: TipoEmpleadosService,
     public loginService: LoginService,
     private router: Router,
     private authService: AuthService
-  ){
+  ) {
     this.loginForm = this.fb.group({
-      correoElectronico: ['', Validators.required],
+      tipoEmpleado: ['', Validators.required],
+      numeroIdentificacion: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
+  ngOnInit(): void {
+    this.tipoEmpleadoService.getAllTipoEmpleados().subscribe(resp => {
+      this.tipoEmpleado = resp.datos
+    },
+      error => { console.error(error) }
+    );
+  }
+
+
   iniciarSesion(): void {
     this.loginService.login(this.loginForm.value).subscribe(
       resp => {
-        if (resp.success) { 
+        if (resp.success) {
           this.authService.login();
           this.router.navigate(['/parqueadero']);
         } else {
           alert(resp.mensajes[0]);
         }
       },
-      error => { 
-        console.error(error); 
+      error => {
+        console.error(error);
         if (error.error && error.error.mensajes) {
           alert(error.error.mensajes[0]);
         } else {
-          alert('Ocurrió un error inesperado. Por favor, intente nuevamente más tarde.'); 
+          alert('Ocurrió un error inesperado. Por favor, intente nuevamente más tarde.');
         }
       }
     );
